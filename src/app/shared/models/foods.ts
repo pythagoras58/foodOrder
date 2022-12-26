dffcb5bcd495c7f9e77a4bb899b2cdc20db5c2f6
pythagoras58 <solomondanso58@gmail.com>
2022-12-26 23:08:50 +0000

    Food Model class

---
+++ src/app/shared/models/foods.ts
@@ -0,0 +1,13 @@
+export class Foods{
+  id! : number;
+  price! : number;
+  name! : number;
+  favorite : boolean = false;
+
+  star : number = 0;
+  tags? : string;
+
+  imageUrl! : string;
+  cookTime !: string;
+  origins!: string[];
+}
