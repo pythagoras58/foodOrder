e6f1d5c0ecd331651e8518c0cc8d872bdc1f301f
pythagoras58 <solomondanso58@gmail.com>
2022-12-26 22:10:14 +0000

    Get all food images in service

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  getAllFoodImages() : string[]{
+    return [
+      '/assets/images/bread-barrel.jpg',
+      '/assets/images/caesar.jpg',
+      '/assets/images/cake.jpg',
+      '/assets/images/greek-salad.jpg',
+      '/assets/images/lobster-bisque.jpg',
+      '/assets/images/lobster-roll.jpg',
+      '/assets/images/mozzarella.jpg',
+      '/assets/images/specials-1.png',
+      '/assets/images/specials-2.png',
+      '/assets/images/specials-3.png',
+      '/assets/images/specials-4.png',
+      '/assets/images/specials-5.png',
+      '/assets/images/lobster-roll.jpg',
+      '/assets/images/tuscan-grilled.jpg',
+      '/assets/images/spinach-salad.jpg'
+    ];
+  }
 }
