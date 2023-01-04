a437faf5d7e0c54fc476b8363773ffb45797aa00
pythagoras58 <solomondanso58@gmail.com>
2023-01-04 23:36:28 +0000

    Cart service methods

--- src/app/services/cart-service.service.ts
+++ src/app/services/cart-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {Cart} from "../shared/models/Cart";
+import {Foods} from "../shared/models/foods";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartServiceService {
 
+  private cart : Cart = new Cart();
   constructor() { }
+
+  addToCart(food : Foods) : void{
+    let cartItem = this.cart.items.find(item=> item.food.id === food.id);
+    if(cartItem){
+      this.changeQuantity(food.id, cartItem.quantity +1);
+      return;
+    }
+    this.cart.items.push(new cartItem(food));
+  }
+
+  removeFromCart(foodId: number): void{
+    this.cart.items = this.cart.items.filter(item => item.food.id != foodId);
+
+  }
+
+  changeQuantity(quantity:number, foodId : number):void{
+
+  }
+
 }
